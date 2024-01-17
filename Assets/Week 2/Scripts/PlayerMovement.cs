94a0a73290b23cd4198fc8c395d474cd3e62a260
DESKTOP-APTFB7I\Joshua <hargobij@sheridancollege.ca>
2024-01-17 14:21:02 -0500

    commit for save

--- Assets/Week 2/Scripts/PlayerMovement.cs
+++ Assets/Week 2/Scripts/PlayerMovement.cs
@@ -29,7 +29,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-      //Debug Log("Player hit the wall");
+        //Debug Log("Player hit the wall");
+        Destroy(gameObject);
             
     }
 }
