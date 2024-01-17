4f28df4b0a7d43c14eee2d45c3c58f39705808c5
DESKTOP-APTFB7I\Joshua <hargobij@sheridancollege.ca>
2024-01-17 14:09:19 -0500

    Task 1 wip

---
+++ Assets/Week 2/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 direction;
+    public Rigidbody2D rigidbody;
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(direction * force * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+      //Debug Log("Player hit the wall");
+            
+    }
+}
