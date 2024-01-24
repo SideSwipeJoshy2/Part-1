711276b0c8c21776bd65fa9a775cbac4e4127887
DESKTOP-APTFB7I\Joshua <hargobij@sheridancollege.ca>
2024-01-24 14:40:11 -0500

    task 1 complete, car moves and border to prevent car from going off track

---
+++ Assets/Week 3/Scripts/Car.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+
+    float accel;
+    float steering;
+    public float forwardSpeed = 500;
+        public float steeringSpeed = 100;
+    Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        accel = Input.GetAxis("Vertical");
+        steering = Input.GetAxis("Horizontal");
+
+
+    }
+
+    public void FixedUpdate()
+    {
+        rigidbody.AddTorque(steering * steeringSpeed * Time.deltaTime);
+        Vector2 force = transform.up * accel* forwardSpeed * Time.deltaTime;
+        rigidbody.AddForce(force);
+    }
+}
